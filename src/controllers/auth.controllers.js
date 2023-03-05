023efd56ef9f3c5bc8ec0bd696ad5c4237f8204a
Shivam Agarwal <itisshivam22@gmail.com>
2023-03-06 01:27:42 +0530

    fix: minor fixes to show proper errors

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -16,23 +16,24 @@ const loginUser = async (req, res) => {
     const { username, password } = req.body;
     try {
         const token = await AuthServices.loginUser(username, password);
-        await client.set("token", token, "EX", 3600);
+        await client.set(token, username, "EX", 3600);
         return res.status(200).json({ message: "Login successful", token});
     }
     catch (err) {
-        return res.status(500).json(err);
+        return res.status(500).json({error : err.message});
     }
 };
 
 const validateToken = async (req, res) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(" ")[1];
+    console.log(token);
     try {
-        const tokenFromRedis = await client.get("token");
-        if (tokenFromRedis !== token) {
+        const tokenFromRedis = await client.get(token);
+        if (!tokenFromRedis) {
             return res.status(401).json({ message: "Unauthorized" });
         }
         const isValid = await AuthServices.validateToken(token);
-        return res.status(200).json(isValid);
+        return res.status(200).json({...isValid, username: tokenFromRedis});
     }
     catch (err) {
         return res.status(500).json(err);
