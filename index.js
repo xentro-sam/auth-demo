872ebc907fcb74fd1236ee93d685dedcccd5b267
Shivam Agarwal <itisshivam22@gmail.com>
2023-03-10 08:44:25 +0530

    feat: add cors

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express();
 const AuthRoutes = require('./src/routers/auth.routes');
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,PUT,POST,DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
